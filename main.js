628affccd318ad2fe711ee26cb791679cadb04cd
Steve Pulec <steve@yipit.com>
2012-03-21 21:28:24 -0400

    fix/ceanup inbox zero checking

--- main.js
+++ main.js
@@ -1,39 +1,34 @@
 console.log("testing");
 var polling_timeout = 2000;
-var prev_count = -1;
+var prev_count_zero = true; // Assume you were inbox zero so we don't firework the first time
 
 function print_mail_count() {
     var frame = top.document.getElementById('canvas_frame');
 
+    // Bug: title goes to 'Inbox (5)'
     var inbox_open = $("#canvas_frame").contents().find("a[title='Inbox']").parent().parent().parent().parent().hasClass("aiq");
     if (!inbox_open) {
         setTimeout('print_mail_count()', polling_timeout);
-        console.log("inbox not open");
+        //console.log("inbox not open");
         return
     }
-    var mail_count_div = $("#canvas_frame").contents().find("div[gh='tm']").find("span[class='Dj']")[0];
     
-    // var mail_count_div = $("#canvas_frame").contents().find("div[aria-expanded='true']")[0];
-    // var mail_count_div2 = $("#canvas_frame").contents().find("span[class='Dj']")[0];
+    var mail_count_div = $("#canvas_frame").contents().find("div[gh='tm']").find("div[class='Di']")[0];
     if (mail_count_div) {
-        var mail_count_text = mail_count_div.children[2].textContent;
-        var mail_count = parseInt(mail_count_text, 10);
-        console.log("mail count set to " + mail_count + "    from: " + mail_count_text);
+        //console.log("couldnt find div, setting to 0");
+        mail_count_zero = true;
     } else {
-        // If there is no div, then inbox is empty
-        console.log("couldnt find div, setting to 0");
-        mail_count = 0;
+        // If there is no div, then inbox is not empty
+        mail_count_zero = false;
     }
     
-    //var mail_count = frame.contentWindow.document.getElementById(':b2').children[0].children[2].textContent;
-    console.log("mail count" + mail_count, "   prev_count:" + prev_count);
-    
-    if (mail_count == 0 && prev_count > 0) {
+    //console.log("mail count: " + mail_count_zero, "   prev_count: " + prev_count_zero);
+    if (mail_count_zero && !prev_count_zero) {
         console.log("at zero");
         start_fireworks();
     }
     
-    prev_count = mail_count;
+    prev_count_zero = mail_count_zero;
     setTimeout('print_mail_count()', polling_timeout);
 }
 setTimeout('print_mail_count()', polling_timeout);
@@ -47,13 +42,3 @@ function start_fireworks() {
         $("#mycanvas").remove();
     });
 }
-
-
-function load_js_file(filename){
-  var script_url = chrome.extension.getURL(filename);
-  var fileref = document.createElement('script');
-  fileref.setAttribute("type", "text/javascript");
-  fileref.setAttribute("src", filename);
-  document.getElementsByTagName("head")[0].appendChild(fileref);
-}
-
